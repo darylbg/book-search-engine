06b1157ce0fd7df1805408fb1dbb1ff1ddaf5559
darylbg <darylxcuf@gmail.com>
2023-07-02 14:48:13 +0100

    set up mongodb atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://darylxcuf:73oBoUaC3OrAcPP0@cluster0.x0g7fmp.mongodb.net/googlebooks')
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(error => console.error('Could not connect to MongoDB Atlas...', error));
 // mongodb://localhost:27017
 // mongodb://localhost:27017
+
 
 module.exports = mongoose.connection;
