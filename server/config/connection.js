2a3ebae16c2531e3bcc7418ba270781215223610
darylbg <darylxcuf@gmail.com>
2023-07-02 15:55:03 +0100

    added typedefs

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
+// set up mongodb atlas as mongodb compass is not working for me
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://darylxcuf:73oBoUaC3OrAcPP0@cluster0.x0g7fmp.mongodb.net/googlebooks')
     .then(() => console.log('Connected to MongoDB Atlas'))
     .catch(error => console.error('Could not connect to MongoDB Atlas...', error));
-// mongodb://localhost:27017
-// mongodb://localhost:27017
 
 
 module.exports = mongoose.connection;
